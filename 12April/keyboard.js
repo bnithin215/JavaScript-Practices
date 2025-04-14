7bcf4256d691f88a433c3d71f5356b6ba89220a2
bnithin215 <nithinbuggargani@gmail.com>
2025-04-14 18:54:29 +0530

    updated

--- 12April/keyboard.js
+++ 12April/keyboard.js
@@ -1,6 +1,18 @@
 let input=document.querySelector('input');
 // input.addEventListener("keypress",function(){
 //     console.log("key was pressed")});
-input.addEventListener("keyup",function(event){
-    console.log(event);
-    console.log("key was pressed")});
+input.addEventListener("keydown",function(event){
+    // console.log(event.key);
+     console.log(event.code);
+    // console.log("key was pressed")});
+
+    if(event.code=="ArrowUp"){
+     console.log("moves Forward");
+    }else if(event.code=="ArrowDown"){
+     console.log("Moves Backward");
+    }else if(event.code=="ArrowLeft"){
+     console.log("Moves left");
+    }else if(event.code=="ArrowRight"){
+     console.log("Moves Right");
+    }
+});
